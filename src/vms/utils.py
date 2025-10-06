64c8ffbb0aae66e40ab5bbe14267f029a5d73fe6
Frankie Robertson <frankie@robertson.name>
2025-10-06 10:07:34 +0300

    Switch to parsing from apitofsim

--- src/vms/utils.py
+++ src/vms/utils.py
@@ -40,41 +40,12 @@ class PairedRangeInputWidget:
         )
 
 
-def read_dat(fn):
-    return [float(line.strip()) for line in open(fn)]
-
-
-def parse_config(fn):
-    config = {}
-    with open(fn) as f:
-        for line in f:
-            line = line.strip()
-            if " " not in line:
-                continue
-            value, name = line.strip().split()
-            config[name] = value
-    result = {"config": config}
-    for particle in ["cluster", "first_product", "second_product"]:
-        particle_data = {}
-        for quantity in [
-            "vibrational_temperatures",
-            "rotational_temperatures",
-            "electronic_energy",
-        ]:
-            config_key = f"file_{quantity}_{particle}"
-            particle_data[quantity] = read_dat(config[config_key])
-            particle_data["name"] = (
-                config[config_key].rsplit(".", 1)[0].rsplit("/", 1)[-1]
-            )
-        particle_data["atomic_mass"] = config[f"Atomic_mass_{particle}"]
-        result[particle] = particle_data
-    return result
-
-
 def parse_config_list(fn):
+    from apitofsim import parse_config_with_particles
+
     with open(fn) as f:
         config_dict = json.load(f)
         for k, conf_info in config_dict.items():
             with chdir(conf_info["cwd"]):
-                config_dict[k] = parse_config(conf_info["config"])
+                config_dict[k] = parse_config_with_particles(conf_info["config"])
     return config_dict
