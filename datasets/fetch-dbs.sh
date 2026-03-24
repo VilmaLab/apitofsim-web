17261baee02babd393cc3bcadc57b8a58e6886cb
Frankie Robertson <frankier@frankier.name>
2026-03-24 12:26:10 +0200

    Update db fetch scripts to only fetch what's needed and print progress

--- datasets/fetch-dbs.sh
+++ datasets/fetch-dbs.sh
@@ -1,3 +1,8 @@
 #!/bin/env bash
 
-cd $1 && RCLONE_S3_ENDPOINT=https://a3s.fi rclone copy :s3,env_auth:apitofsim-data .
+set -euo pipefail
+IFS=$'\n\t'
+
+export RCLONE_S3_ENDPOINT=https://a3s.fi
+rclone copyto --progress :s3,env_auth:apitofsim-data/database.ase.sqlite.db  .
+rclone copyto --progress :s3,env_auth:apitofsim-data/database.duckdb  .
--- fetch-and-start.sh
+++ fetch-and-start.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 IFS=$'\n\t'
 
 export RCLONE_S3_ENDPOINT=https://a3s.fi 
-cd $DATABASE_DIR && rclone copy :s3,env_auth:apitofsim-data .
+cd $DATABASE_DIR && \
+   rclone copyto --progress :s3,env_auth:apitofsim-data/database.ase.sqlite.db  . && \
+   rclone copyto --progress :s3,env_auth:apitofsim-data/database.duckdb  .
 
 export DATABASE="$DATABASE_DIR/database.duckdb"
 exec hypercorn -w 1 -b 0.0.0.0:8080 vms:app
