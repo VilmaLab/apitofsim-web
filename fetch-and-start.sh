4bf0a15d7daf1f644572c34df2235025aaee4c9a
Frankie Robertson <frankier@frankier.name>
2026-09-24 12:57:01 +0300

    Fix prek problems

--- fetch-and-start.sh
+++ fetch-and-start.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export RCLONE_S3_ENDPOINT=https://a3s.fi 
+export RCLONE_S3_ENDPOINT=https://a3s.fi
 cd $DATABASE_DIR && \
    rclone copyto --progress :s3,env_auth:apitofsim-data/database.ase.sqlite.db  . && \
    rclone copyto --progress :s3,env_auth:apitofsim-data/database.duckdb  .
